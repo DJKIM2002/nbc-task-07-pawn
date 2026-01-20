1645c294e3e76f6c5c64c5377a78e9aa6fadf073
djkim12 <agwx1223@naver.com>
2026-01-20 12:47:27 +0900

    Initial project import with assets and configs
    Added initial Unreal Engine project files, including configuration files, character assets, animations, materials, textures, and maps for Drone_Scavenger, ParagonGideon, LP_FPSLite_JC, and Survival_Character content. This sets up the base project structure and required resources for development.

---
+++ Source/NBC_Task_07/NBC_Task_07.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_Task_07.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_Task_07, "NBC_Task_07" );
